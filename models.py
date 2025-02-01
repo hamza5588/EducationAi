38583b1c957350ef883823d82048a276e5d4e8a6
hamza5588 <130641170+hamza5588@users.noreply.github.com>
2025-02-01 21:56:56 +0500

    Update models.py

--- models.py
+++ models.py
@@ -2,13 +2,22 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_groq import ChatGroq
 from config import OPENAI_API_KEY
 from db import get_user_groq_api_key
+from dotenv import load_dotenv
+import logging
 
+logger = logging.getLogger(__name__)
+
+# Load environment variables from .env file if it exists
+load_dotenv()
+
+from langchain_nomic import NomicEmbeddings
 def create_embeddings():
-    return OpenAIEmbeddings(
-        api_key=OPENAI_API_KEY,
-        model="text-embedding-3-small"
-    )
-
+    # OPENAI_API_KEY = "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"
+    return NomicEmbeddings(
+    model="nomic-embed-text-v1.5",
+    # api_key="nk-W3huYcOUS_-GztrGv509-7MxCl8u1GIBRy8hZASQuX0"
+  
+)
 embeddings = create_embeddings()
 
 def get_chat_model(user_id):
